c2ed98543b19dab11d498734655851c1e88c77fe
Nate Arnold <nate@n3wcreation.com>
2011-11-25 22:19:48 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamupp::Application.config.secret_token = '966c6b38775b706260c63b9a94ac515dfaa9efefe6ac63e81992fdd83f888c45fd85386d59b481eb367f7f549243201712343b5d1849820bd276f74a2ed18865'
